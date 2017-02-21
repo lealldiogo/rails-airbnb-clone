4ae7c2567d9234ba46a3690e9dea7c3bc4ae7304
Diogo Loreto Leal <lealldiogo@gmail.com>
2017-02-20 22:56:42 -0300

    fix #2

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170220201125) do
-=======
 ActiveRecord::Schema.define(version: 20170220221301) do
->>>>>>> master
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
+  create_table "bookings", force: :cascade do |t|
+    t.string   "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.float    "price"
     t.text     "description"
     t.string   "category"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-=======
-  create_table "bookings", force: :cascade do |t|
-    t.string   "status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -48,7 +44,6 @@ ActiveRecord::Schema.define(version: 20170220221301) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
->>>>>>> master
   end
 
 end
