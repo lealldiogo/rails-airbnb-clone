77e93479bb1587530b08805aaff14de00056588e
Diogo Loreto Leal <lealldiogo@gmail.com>
2017-02-22 12:51:19 -0300

    Carrierwave set-up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170221181958) do
+ActiveRecord::Schema.define(version: 20170222154906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 20170221181958) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "city"
+    t.string   "photo"
     t.index ["user_id"], name: "index_products_on_user_id", using: :btree
   end
 
