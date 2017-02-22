35989d5b40399ff2ee0bc4d0714e57208e06bafd
Fabio Souza <fabiobarboza7@hotmail.com>
2017-02-22 17:24:42 -0300

    Removed vendor cache

--- vendor/cache/gems/cloudinary-1.1.0/vendor/assets/javascripts/cloudinary/processing.js
+++
@@ -1,5 +0,0 @@
-//= require ./canvas-to-blob.min.js
-//= require ./load-image.min.js
-//= require ./jquery.fileupload-process.js
-//= require ./jquery.fileupload-image.js
-//= require ./jquery.fileupload-validate.js
