d34143a6ab1886b306781b9ec9324f9477575495
Rodrigo Ruas <rodrigoruas2@gmail.com>
2017-02-22 19:40:49 +0100

    19-issue NAO RESOLVIDA

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,32 @@
+class BookingsController < ApplicationController
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: :index
+
+  def index
+    @bookings = Booking.all
+    @user = current_user
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(bookings_params)
+
+    if @booking.save
+      redirect_to user_bookings
+    else
+      render new_user_booking_path
+    end
+
+  end
+
+  def bookings_params
+    params.require(:bookings).permit(:status)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
 
+  resources :users do
+    resources :bookings
+  end
+
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
