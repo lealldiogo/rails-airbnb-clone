fe823069ba5c42ec88c79eff4db588f755d9484e
Diogo Loreto Leal <lealldiogo@gmail.com>
2017-02-23 11:24:01 -0300

    Bookings controller working

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,16 +2,23 @@ class BookingsController < ApplicationController
   # before_action :authenticate_user!
   # skip_before_action :authenticate_user!, only: :index
 
+  # After everything is working, a way to make the app more organized would be to create a before action.
+  # This would force the user to sign in before checking the products and bookings
+
   def index
-    @bookings = Booking.all
+    if user_signed_in?
+      @bookings = current_user.bookings
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def show
-    @booking = Booking.find(params[:id])
-  end
-
-  def new
-    @booking = Booking.new
+    if user_signed_in?
+      @booking = current_user.bookings.find(params[:id])
+    else
+      redirect_to new_user_session_path
+    end
   end
 
   def create
