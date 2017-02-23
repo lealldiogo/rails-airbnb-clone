c5afb9da1edca9e865970590fb572e2951c3c477
Rodrigo Ruas <rodrigoruas2@gmail.com>
2017-02-23 19:42:32 +0100

    Facebook is done

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
 
   resources :bookings
 
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   resources :products do
     collection do
       get 'search'
     end
   end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223012856) do
+ActiveRecord::Schema.define(version: 20170223180835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,12 +45,12 @@ ActiveRecord::Schema.define(version: 20170223012856) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.string   "photo"
     t.string   "brand"
     t.string   "design"
     t.string   "group"
     t.integer  "year"
     t.string   "city"
-    t.string   "photo"
     t.index ["user_id"], name: "index_products_on_user_id", using: :btree
   end
 
@@ -67,6 +67,13 @@ ActiveRecord::Schema.define(version: 20170223012856) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "facebook_picture_url"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
