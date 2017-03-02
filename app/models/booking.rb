f596967e79bd384dd6425bc427f1474d2d3177ad
Fabio Souza <fabiobarboza7@hotmail.com>
2017-03-02 17:48:49 -0300

    added denied button

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ApplicationRecord
-  BOOKING_STATUS = ["negotiating", "deal reached", "rented", "finished"]
+  BOOKING_STATUS = ["negotiating", "deal reached", "rented", "finished", "denied"]
   belongs_to :product
   belongs_to :user
   validates :status, inclusion: { in: BOOKING_STATUS }
